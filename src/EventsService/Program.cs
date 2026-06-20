6f891dd5a1e0b70b4971b1d5e56a1f7a2fe5a28f
blinovapolina <blinovapolina06@yandex.ru>
2026-06-21 00:41:00 +0300

    Refactor EventsService and add exception middleware

--- src/EventsService/Program.cs
+++ src/EventsService/Program.cs
@@ -1,4 +1,6 @@
 using EventsService.Data;
+using EventsService.Middleware;
+using EventsService.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,10 +9,11 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var postgresConnection =
-    builder.Configuration.GetConnectionString("DefaultConnection")
+var postgresConnection = builder.Configuration.GetConnectionString("DefaultConnection")
     ?? "Host=localhost;Port=5432;Database=events_db;Username=ticketuser;Password=ticketpass";
 
+builder.Services.AddDbContext<EventsDbContext>(options =>
+    options.UseNpgsql(postgresConnection));
 
 var redisConnection = builder.Configuration.GetConnectionString("Redis") ?? "localhost:6379";
 builder.Services.AddStackExchangeRedisCache(options =>
@@ -18,14 +21,16 @@ builder.Services.AddStackExchangeRedisCache(options =>
     options.Configuration = redisConnection;
 });
 
-builder.Services.AddDbContext<EventsDbContext>(options =>
-    options.UseNpgsql(postgresConnection));
+builder.Services.AddScoped<EventSeatService>();
+builder.Services.AddScoped<EventCacheService>();
 
 var app = builder.Build();
 
 await DatabaseInitializer.InitializeAsync<EventsDbContext>(
     app.Services,
     async db => await DbSeeder.SeedAsync(db));
+
+app.UseMiddleware<ExceptionMiddleware>();
 
 if (app.Environment.IsDevelopment())
 {
