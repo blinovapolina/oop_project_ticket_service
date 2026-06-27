df3828f6d78b6dca23e0afc1bdfac4d038f18081
blinovapolina <blinovapolina06@yandex.ru>
2026-06-27 11:21:19 +0300

    Fixed Event Service (swagger)

--- src/EventsService/Program.cs
+++ src/EventsService/Program.cs
@@ -2,12 +2,47 @@ using EventsService.Data;
 using EventsService.Middleware;
 using EventsService.Services;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.AddSecurityDefinition("X-User-Role", new OpenApiSecurityScheme
+    {
+        Name = "X-User-Role",
+        Type = SecuritySchemeType.ApiKey,
+        In = ParameterLocation.Header,
+        Description = "User role: 'Admin' for admin endpoints (e.g., POST /api/events)"
+    });
+
+    c.AddSecurityDefinition("X-User-Id", new OpenApiSecurityScheme
+    {
+        Name = "X-User-Id",
+        Type = SecuritySchemeType.ApiKey,
+        In = ParameterLocation.Header,
+        Description = "User ID for authentication"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "X-User-Role"
+                }
+            },
+            new List<string>()
+        }
+    });
+});
 
 var postgresConnection = builder.Configuration.GetConnectionString("DefaultConnection")
     ?? "Host=localhost;Port=5432;Database=events_db;Username=ticketuser;Password=ticketpass";
